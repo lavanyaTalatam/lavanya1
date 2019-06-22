f97748d98fe860e6ab9f18f89a0ab1b5bec5c224
unknown <vvpreethi578@gmail.com>
2019-06-22 10:14:36 +0530

    first commit

---
+++ lavanya1/src/main/java/com/example/lavanya1/Lavanya1Application.java
@@ -0,0 +1,13 @@
+package com.example.lavanya1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lavanya1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lavanya1Application.class, args);
+	}
+
+}
